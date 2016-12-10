101bd910628558c6f563d67d742208d42c2e7e5a
techgaun <coolsamar207@gmail.com>
2016-12-09 20:27:14 -0600

    switch to hex published package

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule XmartThingsDemo.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:xmart_things, github: "techgaun/xmart_things"},
+     {:xmart_things, "~> 0.1"},
      {:httpoison, "~> 0.10"}
    ]
   end
